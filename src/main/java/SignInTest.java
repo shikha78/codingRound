280c092cc61d951cc1699890c1f5fe9987b51ac4
Deepshikha Yadav <deepshikha.yadav78@outlook.com>
2019-10-12 02:28:40 +0530

    SignInTest Fix

--- src/main/java/SignInTest.java
+++ src/main/java/SignInTest.java
@@ -1,26 +1,27 @@
-import com.sun.javafx.PlatformUtil;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import com.sun.javafx.PlatformUtil;
+
 public class SignInTest {
 
-    WebDriver driver = new ChromeDriver();
+	WebDriver driver;
 
     @Test
     public void shouldThrowAnErrorIfSignInDetailsAreMissing() {
 
         setDriverPath();
-
+        driver = new ChromeDriver();
         driver.get("https://www.cleartrip.com/");
         waitFor(2000);
 
         driver.findElement(By.linkText("Your trips")).click();
         driver.findElement(By.id("SignIn")).click();
-
-        driver.findElement(By.id("signInButton")).click();
+        driver.switchTo().frame("modal_window");
+        driver.findElement(By.xpath("//*[@id=\"signInButton\"]")).click();
 
         String errors1 = driver.findElement(By.id("errors1")).getText();
         Assert.assertTrue(errors1.contains("There were errors in your submission"));
